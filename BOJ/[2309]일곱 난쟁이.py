c1bd64f44c1dd56494efd0f5f65736f6da3db6df
bmlsj <gaga2353@gmail.com>
2023-04-03 17:57:40 +0900

    브론즈1 / 완탐

--- BOJ/[2309]일곱 난쟁이.py
+++ BOJ/[2309]일곱 난쟁이.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 
+"""
 nan_jang = []
 for _ in range(9):
     nan_jang.append(int(input()))
@@ -13,4 +14,19 @@ for i in select_seven:
 
 ans.sort()
 for i in ans:
+    print(i)
+"""
+
+height = []
+for _ in range(9):
+    height.append(int(input()))
+
+nanjang = list(combinations(height, 2))
+for i in nanjang:
+    if sum(height) - sum(i) == 100:
+        height.remove(i[0])
+        height.remove(i[1])
+
+height = sorted(height)
+for i in height:
     print(i)
