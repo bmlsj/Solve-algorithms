d75d3266f27a48605672c505477f5403db92e57c
bmlsj <gaga2353@gmail.com>
2023-04-30 14:23:59 +0900

    이진 탐색

--- BOJ/basic/[1920]수 찾기.py
+++ BOJ/basic/[1920]수 찾기.py
@@ -1,13 +1,40 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+#
+# n = int(input())
+# nlist = set(map(int, input().split()))
+#
+# m = int(input())
+# mlist = list(map(int, input().split()))
+#
+# for i in mlist:
+#     if i in nlist:
+#         print(1)
+#     else:
+#         print(0)
+
+def binary_search(target, data, start, end):
+    if start > end:
+        return 0
+
+    mid = (start + end) // 2
+
+    if data[mid] == target:
+        return 1
+    elif data[mid] > target:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+    return binary_search(target, data, start, end)
+
+
 n = int(input())
 nlist = set(map(int, input().split()))
+nlist = sorted(nlist)
 
 m = int(input())
 mlist = list(map(int, input().split()))
 
 for i in mlist:
-    if i in nlist:
-        print(1)
-    else:
-        print(0)
+    print(binary_search(i, nlist, 0, len(nlist)-1))
