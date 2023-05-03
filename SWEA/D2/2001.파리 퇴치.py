df5a64d288f537bfa2d0db38f194e89bf204999e
bmlsj <gaga2353@gmail.com>
2023-05-03 20:28:04 +0900

    SWEA D2

---
+++ SWEA/D2/2001.파리 퇴치.py
@@ -0,0 +1,23 @@
+import sys
+
+sys.stdin = open("../SW 문제해결 기본/input.txt", "r")
+
+t = int(input())
+
+for a in range(1, 11):
+
+    # n : 배열 크기 / m : 파리채 크기
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+    hit = 0
+    for i in range(n - m + 1):
+        for j in range(n - m + 1):
+            pari = 0
+            for k in range(m):
+                for p in range(m):
+                    pari += arr[i + k][j + p]
+
+            hit = max(hit, pari)
+
+    print(f"#{a} {hit}")
