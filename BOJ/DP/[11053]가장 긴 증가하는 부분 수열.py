92bc7b40d669f42fb595e29704a564d6d8660201
bmlsj <gaga2353@gmail.com>
2023-05-09 21:16:33 +0900

    실버2 / DP

---
+++ BOJ/DP/[11053]가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,23 @@
+# # https://pa-pico.tistory.com/83
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0 for _ in range(n)]
+for i in range(n):
+    if i == 0:
+        dp[i] = 1
+    else:
+        max_dp = 0
+        for j in range(i):  # arr[i] 이전 값 중 arr[i]보다 작으면서 가장 큰 dp 값을 가진 j를 찾는다.
+            if arr[i] > arr[j] and max_dp < dp[j]:
+                max_dp = dp[j]
+
+        dp[i] = max_dp + 1
+
+print(dp)
+print(max(dp))
+
+
+
+
