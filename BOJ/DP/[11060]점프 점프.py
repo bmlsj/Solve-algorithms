bf347b9deb09cb71ef02da0533121c2c256d7d75
bmlsj <gaga2353@gmail.com>
2023-05-29 17:57:13 +0900

    실버2 / DP

---
+++ BOJ/DP/[11060]점프 점프.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline()
+
+n = int(input())
+jump = list(map(int, input().split()))
+dp = [0] * n
+
+for i in range(n):
+    for j in range(1, jump[i] + 1):
+        if i + j < n and dp[i+j] == 0:
+            dp[i+j] = dp[i] + 1
+
+if dp[1:].count(0):
+    print(-1)
+else:
+    print(dp[n-1])
