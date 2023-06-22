6e01ceb6efb85e50cff6a6a35942a7eebf127851
bmlsj <gaga2353@gmail.com>
2023-06-22 17:17:28 +0900

    lv2

---
+++ PGS/멀리 뛰기.py
@@ -0,0 +1,20 @@
+def solution(n):
+
+    # 1-> 1
+    # 2 -> 2
+    # 3 -> 3
+    # 4 -> 5
+    # 5 -> 8
+
+    # ak =  ak-1 + k - 1
+    dp = [0 for _ in range(n+1)]
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+
+    print(dp)
+    return dp[n-1]
+
+print(solution(4))
