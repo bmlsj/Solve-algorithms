fc1a45b3affdd525cd77fb16f9fe80cf9828823f
bmlsj <gaga2353@gmail.com>
2023-07-04 17:13:40 +0900

    시간초과 발생

---
+++ BOJ/basic/[1406]에디터.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+
+## 시간 초과 발생
+string_list = list(input())
+cursor = len(string_list)
+
+for _ in range(int(input())):
+    command = list(input().split())
+    if command[0] == 'P':
+        string_list.insert(cursor, command[1])
+        cursor += 1
+
+    elif command[0] == 'L':
+        if cursor > 0:
+            cursor -= 1
+
+    elif command[0] == 'D':
+        if cursor < len(string_list):
+            cursor += 1
+
+    else:
+        if cursor > 0:
+            string_list.remove(string_list[cursor - 1])
+
+print(''.join(string_list))
