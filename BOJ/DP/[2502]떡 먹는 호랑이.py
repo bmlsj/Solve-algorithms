99deb665b197ad4b5b58244cf78c8396b11d5e23
bmlsj <gaga2353@gmail.com>
2023-07-06 22:26:21 +0900

    실버1 / DP

---
+++ BOJ/DP/[2502]떡 먹는 호랑이.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+d, k = map(int, input().split())
+
+dp = [0] * d
+check = False
+
+for i in range(1, k):
+    for j in range(1, k):
+        dp[0], dp[1] = i, j
+        for a in range(2, d):
+            dp[a] = dp[a - 1] + dp[a - 2]
+        if dp[-1] == k:
+            check = True
+            break
+    if check:
+        break
+
+
+print(dp[0])
+print(dp[1])
