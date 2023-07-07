04f5159e56797c8ba2c3b6ae1aaa0df92425f75e
bmlsj <gaga2353@gmail.com>
2023-07-07 17:06:25 +0900

    LCS 알고리즘 추가

--- BOJ/DP/[9251]LCS.py
+++ BOJ/DP/[9251]LCS.py
@@ -1,11 +1,15 @@
 import sys
+
 input = sys.stdin.readline
 
 str1 = input().rstrip()
 str2 = input().rstrip()
 
-cache = [0] * len(str2)
+LCS = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
+## dp 사용
+"""
+cache = [0] * len(str2)
 for i in range(len(str1)):
     cnt = 0
     for j in range(len(str2)):
@@ -16,3 +20,14 @@ for i in range(len(str1)):
             cache[j] = cnt + 1
 
 print(max(cache))
+"""
+
+## LCS 알고리즘
+for i in range(1, len(str1) + 1):
+    for j in range(1, len(str2) + 1):
+        if str1[i - 1] == str2[j - 1]:
+            LCS[i][j] = LCS[i - 1][j - 1] + 1
+        else:
+            LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+
+print(max(map(max, LCS)))
