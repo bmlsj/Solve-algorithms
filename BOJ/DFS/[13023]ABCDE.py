0288e72ee67ef45cd1f6d34c303dded22267a094
bmlsj <gaga2353@gmail.com>
2023-07-10 20:41:42 +0900

    골드5 / DFS

--- BOJ/DFS/[13023]ABCDE.py
+++ BOJ/DFS/[13023]ABCDE.py
@@ -1,39 +1,37 @@
 import sys
 
 input = sys.stdin.readline
-
-
-def dfs(v, depth):
-    global ans
-    visited[v] = True
-
-    if depth >= 4:
-        ans = True
-        return
-    if ans:
-        return
-
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i, depth + 1)
-            visited[i] = False
-
+sys.setrecursionlimit(10 ** 6)
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n)]
+friend = [[] for _ in range(n)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    friend[a].append(b)
+    friend[b].append(a)
 
-print(graph)
-visited = [False] * n
-ans = False
+
+def dfs(v, depth):
+    global check
+    visited[v] = True
+
+    if depth == 5:
+        check = True
+        return
+
+    for t in friend[v]:
+        if not visited[t]:
+            dfs(t, depth + 1)
+            visited[t] = False
+
+
+check = False
+visited = [False for _ in range(n + 1)]
 for i in range(n):
-    dfs(i, 0)
+    dfs(i, 1)
     visited[i] = False
-    if ans:
+    if check:
         break
 
-print(1 if ans else 0)
+print(1 if check else 0)
